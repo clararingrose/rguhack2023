99a0ea16ee740b1137a7d758ede6e4a5969e3d33
clararingrose <2001824@rgu.ac.uk>
2023-02-25 19:34:01 +0000

    update

--- tetrisBlock.cs
+++ tetrisBlock.cs
@@ -5,6 +5,8 @@ using System.Collections;
 public class tetrisBlock : MonoBehaviour {
     //Ininitialise variables
     public GameObject block;
+    private float time = 3.0f;
+    //public bool vertical = false;
 
     void Start(){
     //Give variables values
@@ -12,9 +14,15 @@ public class tetrisBlock : MonoBehaviour {
     }
 
     void Update(){
-    //Processes/Instructions
-        if (Input.GetKey("w")){
-            this.transform.Translate(0f,0.01f,0f);
+        //Processes/Instructions
+        time -= Time.deltaTime;
+
+        if (Input.GetKey("p") && time <= 0f){
+            Rotate();
+        }
+
+        if (Input.GetKey("a")){
+            this.transform.Translate(-0.01f,0f,0f);
 
         }
 
@@ -23,23 +31,36 @@ public class tetrisBlock : MonoBehaviour {
 
         }
 
-        if (Input.GetKey("a")){
-            this.transform.Translate(-0.01f,0f,0f);
+        /**
+        if (vertical = true){
+            if (Input.GetKey("d")){
+                this.transform.Translate(0.1f,0f,0f);
+
+            }
+
+            if (Input.GetKey("a")){
+                this.transform.Translate(-0.1f,0f,0f);
+
+            }
 
         }
+
+        **/
 
         if (Input.GetKey("s")){
             this.transform.Translate(0f,-0.01f,0f);
 
         }
 
+        
 
     }
 
     void Rotate(){
-        if (Input.GetKey("p")){
             this.transform.Rotate(0f,0f,90f);
-
-        }
+            time = 3.0f;
+            //vertical = true;
+    
+    }
 
 }
