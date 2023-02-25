5cf031f194458555025aa9c66826ef4d42482fe2
clararingrose <2001824@rgu.ac.uk>
2023-02-25 16:13:31 +0000

    woooo

--- tetrisBlock.cs
+++ tetrisBlock.cs
@@ -4,6 +4,7 @@ using System.Collections;
 
 public class tetrisBlock : MonoBehaviour {
     //Ininitialise variables
+    public GameObject block;
 
     void Start(){
     //Give variables values
@@ -12,6 +13,26 @@ public class tetrisBlock : MonoBehaviour {
 
     void Update(){
     //Processes/Instructions
+        if (Input.GetKey("w")){
+            this.transform.Translate(0f,0.01f,0f);
+
+        }
+
+        if (Input.GetKey("d")){
+            this.transform.Translate(0.01f,0f,0f);
+
+        }
+
+        if (Input.GetKey("a")){
+            this.transform.Translate(-0.01f,0f,0f);
+
+        }
+
+        if (Input.GetKey("s")){
+            this.transform.Translate(0f,-0.01f,0f);
+
+        }
+
 
     }
 
